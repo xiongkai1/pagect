933b1750130c8e4903d4f38bc25aa19dfa2feb18
xiongkai <1756110367@qq.com>
2021-03-01 09:49:35 +0800

    商品id查询，店铺商品信息分页查询以及照片分片上传

---
+++ src/services/commodityinfo.js
@@ -0,0 +1,43 @@
+import http, { prepare } from 'Utils/http';
+import { HttpMethod } from 'Constants/enum';
+
+/**
+ * 商品id查询
+ */
+// 通过 post data 传参
+export function shopCommodityIdQuery(data) {
+    return http({
+        url: '/shopping-center/commodityinfo/shopCommodityIdQuery',
+        method: HttpMethod.GET,
+        params: {
+            data
+        }
+    });
+}
+// 店铺商品信息分页查询
+export function shopCommodityList(data) {
+    return prepare({
+        url: '/shopping-center/commodityinfo/shopCommodityList',
+        method: HttpMethod.GET,
+        params: {
+            data
+        }
+    });
+}
+
+/**
+ * 上下架商品
+ * @param {*} data 
+ */
+export function shopCommodityUpAndDownFrame(data) {
+    return prepare({
+        url: '/shopping-center/commodityinfo/shopCommodityUpAndDownFrame',
+        method: HttpMethod.PUT,
+        params: {
+            data
+        }
+    });
+}
+
+
+
